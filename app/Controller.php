856572ea485d10c33b2f57d17ecc513661a5f77e
mbarek-hani <mbarekhani30@gmail.com>
2025-06-24 21:43:18 +0100

    extract database conf to constants

--- app/Controller.php
+++ app/Controller.php
@@ -4,7 +4,7 @@ class Controller {
     protected $pdo;
     public function __construct() {
         try {
-            $this->pdo = new PDO("mysql:host=localhost;dbname=web_project","root","password");
+            $this->pdo = new PDO("mysql:host=". DBHOST .";dbname=". DBNAME,DBUSER,DBPASS);
         } catch (PDOException $e) {
             die("Failed to connect to db" . $e->getMessage());
         }
--- app/init.php
+++ app/init.php
@@ -1,3 +1,7 @@
 <?php
+define("DBHOST", "localhost");
+define("DBNAME", "your_database_name");
+define("DBUSER", "database_username");
+define("DBPASS", "database_password");
 
 require_once "Router.php";
